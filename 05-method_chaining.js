e1238738399a1b2c74e855bfebd4445ff19383da
ArmanAgwan <armanagwa@gmail.com>
2024-02-16 11:39:16 +0530

    Method Chaining

---
+++ 05-method_chaining.js
@@ -0,0 +1,31 @@
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+}
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariorp.com', 'Yoshi');
+
+userOne.login().updateScore().logout();
+
+
+//--------//
+
+console.log(userOne.updateScore());
+console.log(userTwo.updateScore());
